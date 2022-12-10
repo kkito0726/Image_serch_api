9b9ca37f77efd315cceada979ceda731589cda0b
kkito0726 <k.kito0726@gmail.com>
2022-12-10 13:30:05 +0900

    first commit

---
+++ components/ImageComponent.jsx
@@ -0,0 +1,18 @@
+// 画像コンポーネント
+const ImageComponent = (props) => {
+  // ========== EXTRA_TODO: ImageContainerから渡される「ImageComponentをクリックした時に発火する関数」を画像のクリックイベントに割り当て ==========
+  const { url, name, imageId } = props;
+
+  return (
+    <img
+      src={url}
+      alt={name}
+      title={name}
+      image_id={imageId}
+      width="100%"
+      height="100%"
+    />
+  );
+};
+
+window.ImageComponent = ImageComponent;
